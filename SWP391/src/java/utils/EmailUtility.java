e67b926ae30ae648019ed33a03002690a3d11a7e
Twannnnnn <hkt93885@gmail.com>
2024-09-25 01:38:24 +0700

    finish ForgotPassword

--- SWP391/src/java/utils/EmailUtility.java
+++ SWP391/src/java/utils/EmailUtility.java
@@ -3,8 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package utils;
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.mail.*;
 import javax.mail.internet.*;
 
@@ -14,8 +17,11 @@ import javax.mail.internet.*;
  */
 public class EmailUtility {
     
-    public static void sendEmail(String fromEmail, String password, String toEmail, String subject, String htmlContent) throws MessagingException {
+    public static void sendEmail(String toEmail, String subject, String Content) throws MessagingException {
         // Set properties for the mail server
+        String fromEmail = "5englandstrong@gmail.com";
+        String password = "kdoa ghav dooo dopd";
+        
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587"); // For TLS
@@ -34,10 +40,31 @@ public class EmailUtility {
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(fromEmail));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
-        message.setSubject(subject);
+        
+        subject = "[5 Anh Lực] " + subject;
+        
+        // Encode the subject with UTF-8
+        String encodedSubject;
+        try {
+            encodedSubject = MimeUtility.encodeText(subject, "UTF-8", "B");
+            message.setSubject(encodedSubject);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(EmailUtility.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         // Set the email content (HTML format)
-        message.setContent(htmlContent, "text/html");
+        String htmlContent = "<html lang=\"en\">";
+        htmlContent += "<body>";
+        htmlContent += "<h2>Quán ăn 5 Anh Lực gửi</h2>";
+        htmlContent += "<h1>" + Content + "</h1>";
+        htmlContent += "<h2 style=\"color: red; text-transform: uppercase;\">Đây là email tự động, xin đừng trả lời email này</h2>";
+        htmlContent += "</body>";
+        htmlContent += "</html>";
+        
+        
+        // Set the email content (HTML format) and specify UTF-8 encoding
+        message.setContent(htmlContent, "text/html; charset=UTF-8");
+
 
         // Send the email
         Transport.send(message);
